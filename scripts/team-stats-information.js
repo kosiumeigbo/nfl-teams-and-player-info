a161971e366e383064b419fba9c3fa2fa2755d46
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-07-29 11:11:58 +0100

    add functionality for when user reloads or refreshes page

--- scripts/team-stats-information.js
+++ scripts/team-stats-information.js
@@ -1,5 +1,24 @@
 "use strict";
+import * as apiTestData from "../modules/api-test-data.js";
 
-const pageBody = document.body;
+// let teamKey;
 
-pageBody.textContent = localStorage.getItem("teamKey");
+if (
+  window.performance.getEntriesByType("navigation")[0].type === "navigate" ||
+  window.performance.getEntriesByType("navigation")[0].type === "reload"
+) {
+  const teamKey = localStorage.getItem("teamKeyTab");
+  document.body.dataset.key = teamKey;
+  document.body.textContent = teamKey;
+}
+
+document.addEventListener("visibilitychange", function () {
+  if (!document.hidden) {
+    localStorage.setItem("teamKeyTab", document.body.dataset.key);
+  }
+});
+// else {
+//   teamKey = localStorage.getItem("teamKeyTab");
+//   document.body.dataset.key = teamKey;
+//   document.body.textContent = teamKey;
+// }
