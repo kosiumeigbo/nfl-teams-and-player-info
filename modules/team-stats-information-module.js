fee3c76cb9c7666370007dacc185f01044b632ac
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-08-03 08:41:29 +0100

    add team page header functions and variables to team page module

--- modules/team-stats-information-module.js
+++ modules/team-stats-information-module.js
@@ -1,1 +1,17 @@
+export const main = document.createElement("main");
+export const header = document.createElement("header");
 
+export const headerHTML = function (teamObj) {
+  return `
+<div class="header-container">
+  <h1>${teamObj.FullName}</h1>
+  <p>${teamObj.Conference} ${teamObj.Division}</p>
+</div>
+<div class="header-color"></div>
+<div style="background-color: #${teamObj.SecondaryColor}" class="secondary-color"></div>
+<div style="background-color: #${teamObj.PrimaryColor}" class="primary-color"></div>
+<div class="team-logo">
+  <img src="${teamObj.WikipediaLogoUrl}" alt="" />
+</div>
+    `;
+};
