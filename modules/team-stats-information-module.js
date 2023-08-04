fff1a513c64d88f94ec10498afa22387b522bd6b
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-08-04 10:37:10 +0100

    edit headerHTML structure

--- modules/team-stats-information-module.js
+++ modules/team-stats-information-module.js
@@ -1,5 +1,4 @@
 /* Main Part of Page */
-
 export const main = document.createElement("main");
 
 /* Header Section */
@@ -8,14 +7,18 @@ export const header = document.createElement("header");
 export const headerHTML = function (teamObj) {
   return `
 <div class="header-container">
-  <h1>${teamObj.FullName}</h1>
-  <p>${teamObj.Conference} ${teamObj.Division}</p>
+  <div class="name-conference-division">
+    <h1>${teamObj.FullName}</h1>
+    <h3>${teamObj.Conference} ${teamObj.Division}</h3>
+  </div>
+  <div class="team-logo">
+    <img src="${teamObj.WikipediaLogoUrl}" alt="" />
+  </div>
 </div>
 <div class="header-color"></div>
-<div class="team-logo">
-  <img src="${teamObj.WikipediaLogoUrl}" alt="" />
-</div>
 <div style="background-color: #${teamObj.SecondaryColor}" class="secondary-color"></div>
 <div style="background-color: #${teamObj.PrimaryColor}" class="primary-color"></div>
     `;
 };
+
+/* Nav Section */
