039e340401f647d38b8a6035d9e633421217345c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-08-07 23:41:28 +0100

    add event listener logic to fetch NFL API data from server

--- scripts/team-stats-information.js
+++ scripts/team-stats-information.js
@@ -13,6 +13,93 @@ if (teamPageQueryParams.has("key")) {
   window.location.href = "../index.html";
 }
 
+// Event Listener to fetch api to get selected team
+window.addEventListener("DOMContentLoaded", function (e) {
+  fetch("https://api.sportsdata.io/v3/nfl/scores/json/Teams?key=ffb7852aadbe4662a351fad874b411ce")
+    .then((res) => res.json())
+    .then((data) => {
+      team = data.find((obj) => obj.Key === teamKey);
+      console.log(team.FullName);
+
+      document.querySelector("title").textContent = `${team.FullName}`;
+
+      // Create and Insert Main section to Body
+      const main = document.createElement("main");
+      document.body.insertAdjacentElement("afterbegin", main);
+
+      // Create and fill header and append to main
+      const header = document.createElement("header");
+      header.innerHTML = teamPage.headerHTML(team);
+      main.appendChild(header);
+
+      // Create navLinks section with nav-links class, add HTML and append to main
+      const navLinks = document.createElement("section");
+      navLinks.classList.add("nav-links");
+      navLinks.innerHTML = teamPage.navLinksHTML();
+      main.appendChild(navLinks);
+
+      // Create teamInfo section and add all necessary HTML
+      const teamInfoSection = document.createElement("section");
+      teamInfoSection.classList.add("team-info");
+
+      const teamInfoSectionContainer = document.createElement("div");
+      teamInfoSectionContainer.classList.add("team-info-container");
+
+      const [coachingDetailsSection, stadiumDetailsSection] = [
+        document.createElement("div"),
+        document.createElement("div")
+      ];
+      coachingDetailsSection.classList.add("cards-section");
+      stadiumDetailsSection.classList.add("cards-section");
+
+      coachingDetailsSection.insertAdjacentHTML("afterbegin", teamPage.coachingDetailsHTML(team));
+      stadiumDetailsSection.insertAdjacentHTML("afterbegin", teamPage.stadiumDetailsHTML(team));
+
+      teamInfoSectionContainer.appendChild(coachingDetailsSection);
+      teamInfoSectionContainer.appendChild(stadiumDetailsSection);
+      teamInfoSection.appendChild(teamInfoSectionContainer);
+      main.appendChild(teamInfoSection);
+
+      // Create playerList section and add all necessary HTML
+      const teamRosterSection = document.createElement("section");
+      teamRosterSection.classList.add("team-roster");
+      teamRosterSection.classList.add("hidden");
+      const teamRosterSectionContainer = document.createElement("div");
+      teamRosterSectionContainer.classList.add("team-roster-container");
+      teamRosterSectionContainer.insertAdjacentHTML("afterbegin", teamPage.teamRosterHeadings());
+      const teamRosterRows = document.createElement("div");
+      teamRosterRows.classList.add("team-roster-grid");
+      teamRosterRows.classList.add("rows");
+      teamRosterSectionContainer.insertAdjacentElement("beforeend", teamRosterRows);
+      teamRosterSection.appendChild(teamRosterSectionContainer);
+      main.appendChild(teamRosterSection);
+
+      return fetch(
+        `https://api.sportsdata.io/v3/nfl/scores/json/Players/${team.Key}?key=ffb7852aadbe4662a351fad874b411ce`
+      );
+    })
+    .then((res) => res.json())
+    .then((players) => {
+      console.log(players);
+
+      const navButtons = document.querySelector(".nav-links");
+
+      navButtons.addEventListener("click", function (e) {
+        if (e.target.dataset.btn === "team-info") {
+          document.querySelector(".team-roster").classList.add("hidden");
+          document.querySelector(".team-info").classList.remove("hidden");
+        }
+
+        if (e.target.dataset.btn === "team-roster") {
+          document.querySelector(".team-info").classList.add("hidden");
+          document.querySelector(".team-roster").classList.remove("hidden");
+        }
+      });
+    });
+});
+
+/////////////////////////////////////////////////////////////////////////////
+
 const testTeam = apiTestData.allTeamsActive.filter((obj) => obj.Key === teamKey)[0];
 console.log(testTeam);
 
