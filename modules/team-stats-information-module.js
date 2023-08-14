7b089cf977e460997d7757f2fa3bc56a6f41a68d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-08-14 22:15:43 +0100

    add ternary operator for when playerObj Number property is null

--- modules/team-stats-information-module.js
+++ modules/team-stats-information-module.js
@@ -139,7 +139,7 @@ export const otherPlayerInfoTableHeading = function () {
 export const otherPlayerInfoTableRow = function (playerObj) {
   return `
   <tr class="row">
-    <td>${playerObj.Number}</td>
+    <td>${playerObj.Number ? playerObj.Number : 'N/A'}</td>
     <td>${playerObj.Position}</td>
     <td>${playerObj.Active ? "ACT" : "NON-ACT"}</td>
     <td>${playerObj.HeightFeet}ft ${playerObj.HeightInches}in</td>
