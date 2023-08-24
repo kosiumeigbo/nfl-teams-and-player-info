84fa4f39b8b9becd46cd93793f96ab49217c7550
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-08-24 20:03:20 +0100

    edit player page javascript file

--- scripts/player-stats-information.js
+++ scripts/player-stats-information.js
@@ -1,3 +1,5 @@
-import { playerDetailsByPlayer } from "../modules/api-test-data.js";
+import { playerDetailsByPlayer, allTeamsActive } from "../modules/api-test-data.js";
+import {} from "../modules/player-stats-information-module.js";
 
 console.log(playerDetailsByPlayer);
+console.log(allTeamsActive[3]);
