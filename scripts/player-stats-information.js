a2d2fbdb6d98d7426fbf3d093f96c83599801327
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-08-26 02:52:41 +0100

    add code to test out imported headerSection function

--- scripts/player-stats-information.js
+++ scripts/player-stats-information.js
@@ -1,5 +1,15 @@
 import { playerDetailsByPlayer, allTeamsActive } from "../modules/api-test-data.js";
-import {} from "../modules/player-stats-information-module.js";
+import { headerHTML, headerSection } from "../modules/player-stats-information-module.js";
 
-console.log(playerDetailsByPlayer);
-console.log(allTeamsActive[3]);
+const playerPageQueryList = window.location.search;
+const playerPageQueryParams = new URLSearchParams(playerPageQueryList);
+let playerKey;
+
+if (playerPageQueryParams.has("key")) {
+  playerKey = playerPageQueryParams.get("key");
+  console.log(playerKey);
+} else {
+  window.location.href = "../index.html";
+}
+
+document.body.appendChild(headerSection(playerDetailsByPlayer, allTeamsActive[10]));
