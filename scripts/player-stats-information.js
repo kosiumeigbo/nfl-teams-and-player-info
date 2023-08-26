47e5fd7413295929b72f901b7e29ed2f31eec6f3
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-08-26 14:02:15 +0100

    update player page script file

--- scripts/player-stats-information.js
+++ scripts/player-stats-information.js
@@ -1,15 +1,32 @@
 import { playerDetailsByPlayer, allTeamsActive } from "../modules/api-test-data.js";
-import { headerHTML, headerSection } from "../modules/player-stats-information-module.js";
+import {
+  headerSection,
+  playerInfoSection,
+  buildPlayerPage
+} from "../modules/player-stats-information-module.js";
 
 const playerPageQueryList = window.location.search;
 const playerPageQueryParams = new URLSearchParams(playerPageQueryList);
-let playerKey;
+let playerKey, teamKey;
+
+const [player, team] = [playerDetailsByPlayer, allTeamsActive[20]];
+console.log(player);
+console.log(team);
 
 if (playerPageQueryParams.has("key")) {
   playerKey = playerPageQueryParams.get("key");
+  teamKey = playerPageQueryParams.get("team");
   console.log(playerKey);
+  console.log(teamKey);
 } else {
   window.location.href = "../index.html";
 }
 
-document.body.appendChild(headerSection(playerDetailsByPlayer, allTeamsActive[10]));
+// document.body.appendChild(headerSection(player, team));
+// document.body.appendChild(playerInfoSection(player));
+
+window.addEventListener("DOMContentLoaded", function () {
+  buildPlayerPage(playerKey, teamKey).then((mainSection) => {
+    document.body.insertAdjacentElement("afterbegin", mainSection);
+  });
+});
