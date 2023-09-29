9ecf6ac7df55970614b52efa0d445d5a705e7bfe
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-09-29 22:18:32 +0100

    remove playerDetailsByPlayer and allTeamsActive references

--- scripts/player-stats-information.js
+++ scripts/player-stats-information.js
@@ -4,10 +4,6 @@ const playerPageQueryList = window.location.search;
 const playerPageQueryParams = new URLSearchParams(playerPageQueryList);
 let playerKey, teamKey;
 
-const [player, team] = [playerDetailsByPlayer, allTeamsActive[20]];
-console.log(player);
-console.log(team);
-
 if (playerPageQueryParams.has("key")) {
   playerKey = playerPageQueryParams.get("key");
   teamKey = playerPageQueryParams.get("team");
